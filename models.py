0bf19a89916e8a24f78524e9dd40dbf820aed24e
BayTown <andi86h@outlook.com>
2020-03-07 18:40:07 +0100

    Model defined and added / gitignore modified

--- models.py
+++ models.py
@@ -21,17 +21,39 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 64, 5)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
+        self.pool = nn.MaxPool2d(2, 2)
 
+        self.fc1 = nn.Linear(12000, 2048)
+        self.fc2 = nn.Linear(2048, 136)
+
+        self.dropout = nn.Dropout(0.3)
         
+
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
         
-        
+        # Flatten layer to convert the pooled feature maps to a single vectorn
+        x = x.view(x.size()[0], -1)
+
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
